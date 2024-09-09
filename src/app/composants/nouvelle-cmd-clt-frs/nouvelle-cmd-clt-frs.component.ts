4adfab3ee7aece8e78dfcc1db36f82aeddc91945
JradChaima1 <chaimajrad1888@gmail.com>
2024-09-09 19:45:01 +0100

    interface nouvelle commande

--- src/app/composants/nouvelle-cmd-clt-frs/nouvelle-cmd-clt-frs.component.ts
+++ src/app/composants/nouvelle-cmd-clt-frs/nouvelle-cmd-clt-frs.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {faCircleInfo, faPenToSquare, faSquarePen, faTrash, faPlus} from "@fortawesome/free-solid-svg-icons";
+
 
 @Component({
   selector: 'app-nouvelle-cmd-clt-frs',
@@ -6,5 +8,9 @@ import { Component } from '@angular/core';
   styleUrl: './nouvelle-cmd-clt-frs.component.scss'
 })
 export class NouvelleCmdCltFrsComponent {
-
+  protected readonly faSquarePen = faSquarePen;
+  protected readonly faPenToSquare = faPenToSquare;
+  protected readonly faCircleInfo = faCircleInfo;
+  protected readonly faTrash = faTrash;
+  protected readonly faPlus = faPlus;
 }
